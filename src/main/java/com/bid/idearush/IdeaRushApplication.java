8826b375112a4a25504040c0d7ef974cb91e6cf2
ironprayer <ironprayer1208@gmail.com>
2023-07-29 17:19:19 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/bid/idearush/IdeaRushApplication.java
@@ -0,0 +1,13 @@
+package com.bid.idearush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaRushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaRushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bid/idearush/IdeaRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bid.idearush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaRushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
