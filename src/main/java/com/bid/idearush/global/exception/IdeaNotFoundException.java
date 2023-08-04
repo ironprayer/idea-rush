769384a2fe6b8cf486b106ecb142ceba113b4e7e
NHclub <sbe4488@naver.com>
2023-08-04 14:11:21 +0900

    test : 경매예약 글로벌 예외처리 추가

---
+++ src/main/java/com/bid/idearush/global/exception/IdeaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bid.idearush.global.exception;
+
+public class IdeaNotFoundException extends RuntimeException {
+    public IdeaNotFoundException(String message) {
+        super(message);
+    }
+}
