1c5eda721f0aa9a2ab6288c45fadefc343f6350d
ironprayer <ironprayer1208@gmail.com>
2023-08-05 16:41:09 +0900

    feat : S3 업로드 기능 구현

---
+++ src/main/java/com/bid/idearush/global/exception/errortype/FileWriteErrorCode.java
@@ -0,0 +1,17 @@
+package com.bid.idearush.global.exception.errortype;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum FileWriteErrorCode {
+
+    S3_NOT_WRITE(HttpStatus.BAD_REQUEST, "S3 이미지 업로드에 실패했습니다"),
+    ;
+
+    private final HttpStatus status;
+    private final String msg;
+
+}
