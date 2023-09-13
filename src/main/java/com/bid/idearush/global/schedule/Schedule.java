a09aedc347b40b278b31dd519a53d703af139c95
ironprayer <ironprayer1208@gmail.com>
2023-09-13 21:18:24 +0900

    feat : shedlock 적용

--- src/main/java/com/bid/idearush/global/schedule/Schedule.java
+++ src/main/java/com/bid/idearush/global/schedule/Schedule.java
@@ -6,6 +6,7 @@ import com.bid.idearush.domain.idea.model.entity.Idea;
 import com.bid.idearush.domain.idea.repository.IdeaRepository;
 import com.bid.idearush.global.util.NoticeService;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,8 +24,10 @@ public class Schedule {
     private final NoticeService noticeService;
 
     @Scheduled(cron = "0 * * * * *")
+    @SchedulerLock(name = "endBidOfIdea", lockAtMostFor = "50s", lockAtLeastFor = "50s")
     @Transactional
     public void endBidOfIdea() {
+        System.out.println("endBidOfIdea Start");
         List<Idea> ideas =  ideaRepository.findEndIdeas();
 
         for(Idea idea : ideas) {
@@ -36,23 +39,27 @@ public class Schedule {
                 idea.updateBidFail();
             }
 
-            noticeService.noticeBidEndEvent(userId, idea);
+//            noticeService.noticeBidEndEvent(userId, idea);
         }
     }
 
     @Scheduled(cron = "0 * * * * *")
+    @SchedulerLock(name = "beforeTimeBidOfIdea", lockAtMostFor = "50s", lockAtLeastFor = "50s")
     public void beforeTimeBidOfIdea() {
+        System.out.println("beforeTimeBidOfIdea Start");
         int[] hopeTimes = {10, 20, 30};
         LocalDateTime currentTime =  LocalDateTime.now();
 
         for(int hopeTime : hopeTimes){
-            noticeService.noticeBeforeEvent(currentTime, hopeTime);
+//            noticeService.noticeBeforeEvent(currentTime, hopeTime);
         }
     }
 
     @Scheduled(cron = "0 * * * * *")
+    @SchedulerLock(name = "startBidOfIdea", lockAtMostFor = "50s", lockAtLeastFor = "50s")
     @Transactional
     public void startBidOfIdea() {
+        System.out.println("startBidOfIdea Start");
         LocalDateTime currentTime = LocalDateTime.now();
         ideaRepository.updatePrepareToOngoing(currentTime);
     }
